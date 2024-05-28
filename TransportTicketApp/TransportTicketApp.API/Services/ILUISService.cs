0166be02d06d8e0bcb6f7c4ab7663b962191b4e7
CemYasinUzum <cem.uzum@btyon.com>
2024-05-28 19:20:50 +0300

    ai implemented

---
+++ TransportTicketApp/TransportTicketApp.API/Services/ILUISService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+
+namespace Transport.API.Services
+{
+    public interface ILuisService
+    {
+        Task<PredictionResponse> PredictAsync(string query);
+    }
+
+    public class LuisService : ILuisService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly LUISRuntimeClient _client;
+        private readonly string _appId;
+
+        public LuisService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _appId = _configuration["Luis:AppId"];
+            var endpointKey = _configuration["Luis:EndpointKey"];
+            var endpoint = _configuration["Luis:Endpoint"];
+
+            _client = new LUISRuntimeClient(new ApiKeyServiceClientCredentials(endpointKey))
+            {
+                Endpoint = endpoint
+            };
+        }
+
+        public async Task<PredictionResponse> PredictAsync(string query)
+        {
+            var predictionRequest = new PredictionRequest { Query = query };
+            return await _client.Prediction.GetSlotPredictionAsync(Guid.Parse(_appId), "production", predictionRequest);
+        }
+    }
+
+}
