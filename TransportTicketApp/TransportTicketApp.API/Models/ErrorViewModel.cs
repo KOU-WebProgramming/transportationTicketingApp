8b5983670e5e857b08167431b10ab0da3f2e25b4
ibrahim altıparmak <ibrahim.altiparmak2001@gmail.com>
2024-05-28 15:42:28 +0300

    .
    .

---
+++ TransportTicketApp/TransportTicketApp.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransportTicketApp.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
